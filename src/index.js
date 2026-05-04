c0683eb94eb2d9c558e3c1c3f605ed1be38f219f
brett <brett.jewell@librestream.com>
2026-05-04 11:22:16 -0500

    add goodbye button

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ document.getElementById("helloBtn").addEventListener("click", () => {
   const name = document.getElementById("nameInput").value.trim();
   alert(name ? `Hello, ${name}` : "Hello");
 });
+
+document.getElementById("goodbyeBtn").addEventListener("click", () => {
+  const name = document.getElementById("nameInput").value.trim();
+  alert(name ? `Goodbye ${name}` : "Goodbye");
+});
