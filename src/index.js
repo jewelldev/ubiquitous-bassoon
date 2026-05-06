edb9055b5862ff0883549efe7aac02dba4cf43f5
brett <brett.jewell@librestream.com>
2026-05-06 12:10:15 -0500

    Add gitignore and blazor app

--- src/index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("helloBtn").addEventListener("click", () => {
-  const name = document.getElementById("nameInput").value.trim();
-  alert(name ? `Hello, ${name}` : "Hello");
-});
